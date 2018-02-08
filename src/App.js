8f87338111e1211a644bc1c0a3fbd8dc08d6bb1b
Thiago Vicente <thiagovice@gmail.com>
2018-02-08 12:37:37 -0500

    First Commit: create-react-app base

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+	<h2>Learn Path</h2>
+    );
+  }
+}
+
+export default App;
