6ebc355e3f443145332670486cf0c7f0dd25da0c
Thiago Vicente <thiagovice@gmail.com>
2018-04-01 08:51:55 -0400

    Fixed private flag

--- src/components/PathSlider/NewPaths.js
+++ src/components/PathSlider/NewPaths.js
@@ -10,7 +10,7 @@ export default compose(
     return [
       {
         collection: "paths",
-        where: ["private", "==", "false"],
+        where: ["private", "==", false],
         limit: 10,
         orderBy: ["created", "desc"]
       }
