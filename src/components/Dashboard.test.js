2c4cf3598f26cefda02db901b811f1994ea6a3f2
Thiago Vicente <thiagovice@gmail.com>
2018-04-08 09:14:36 -0400

    Temove Dashboard Test

--- src/components/Dashboard.test.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Dashboard from "./Dashboard";
-import { shallow } from "enzyme";
-
-test("should render Home without data", () => {
-  const wrapper = shallow(<Dashboard />);
-});
