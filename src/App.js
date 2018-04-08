d012c18bc38c35c844f66d6fa7926762123f4591
Otaviano Pires <otavianopires@gmail.com>
2018-04-08 10:58:50 -0400

    Added react-burger-menu component and CSS tweaks

--- src/App.js
+++ src/App.js
@@ -12,16 +12,81 @@ import { firebaseConnect } from "react-redux-firebase";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import "./App.css";
+import { slide as Menu } from 'react-burger-menu'
 
 class App extends Component {
   logout = () => {
     this.props.firebase.auth().signOut();
   };
 
+  constructor (props) {
+    super(props)
+    this.state = {
+      menuOpen: false
+    }
+  }
+
+  // This keeps your state in sync with the opening/closing of the menu
+  // via the default means, e.g. clicking the X, pressing the ESC key etc.
+  handleStateChange (state) {
+    this.setState({menuOpen: state.isOpen})  
+  }
+  
+  // This can be used to close the menu, e.g. when a user clicks a menu item
+  closeMenu () {
+    this.setState({menuOpen: false})
+  }
+
+  // This can be used to toggle the menu, e.g. when using a custom icon
+  // Tip: You probably want to hide either/both default icons if using a custom icon
+  // See https://github.com/negomi/react-burger-menu#custom-icons
+  toggleMenu () {
+    this.setState({menuOpen: !this.state.menuOpen})
+  }
+
   render() {
     return (
       <div>
         <header className="nav">
+          <Menu 
+            isOpen={this.state.menuOpen}
+            onStateChange={(state) => this.handleStateChange(state)}
+          >
+            {this.props.auth.isEmpty ? (
+              <Link className="menu-item" to="/login" onClick={() => this.closeMenu()}>Login</Link>
+            ) : (
+              <div>
+                <div
+                  style={{
+                    position: "relative",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexShrink: 0,
+                    width: 60,
+                    height: 60,
+                    borderRadius: "50%",
+                    overflow: "hidden",
+                    userSelect: "none",
+                    marginBottom: "15px"
+                  }}
+                >
+                  <img
+                    alt="LOGOUT"
+                    src={this.props.auth.photoURL}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      textAlign: "center",
+                      objectFit: "cover"
+                    }}
+                  />
+                </div>
+                <Link className="menu-item" to="/dashboard" onClick={() => this.closeMenu()}>Dashboard</Link>
+                <Link className="menu-item" to="/" onClick={this.logout}>Logout</Link>
+              </div>
+            )}
+          </Menu>
           <Link to="/" className="logo">
             Learn Path
           </Link>
@@ -44,7 +109,7 @@ class App extends Component {
                 }}
               >
                 <img
-                  onClick={this.logout}
+                  //onClick={this.logout}
                   alt="LOGOUT"
                   src={this.props.auth.photoURL}
                   style={{
