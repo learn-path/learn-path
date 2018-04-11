ce55885575bdbc80a32c9dce850fbd6eb1bad8ab
Thiago Vicente <thiagovice@gmail.com>
2018-04-10 23:23:23 -0400

    Added cloud function to send paths to Algolia

--- functions/index.js
+++ functions/index.js
@@ -36,3 +36,84 @@ exports.onPathChange = functions.firestore
     console.log(`updating path ${path.objectID} in search index`);
     return index.saveObject(path);
   });
+
+// [START get_firebase_user]
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+function getFirebaseUser(req, res, next) {
+  console.log("Check if request is authorized with Firebase ID token");
+
+  if (
+    !req.headers.authorization ||
+    !req.headers.authorization.startsWith("Bearer ")
+  ) {
+    console.error(
+      "No Firebase ID token was passed as a Bearer token in the Authorization header.",
+      "Make sure you authorize your request by providing the following HTTP header:",
+      "Authorization: Bearer <Firebase ID Token>"
+    );
+    return res.sendStatus(403);
+  }
+
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    console.log("Found 'Authorization' header");
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  }
+
+  return admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedIdToken => {
+      console.log("ID Token correctly decoded", decodedIdToken);
+      req.user = decodedIdToken;
+      return next();
+    })
+    .catch(error => {
+      console.error("Error while verifying Firebase ID token:", error);
+      return res.status(403).send("Unauthorized");
+    });
+}
+// [END get_firebase_user]
+
+// [START get_algolia_user_token]
+// This complex HTTP function will be created as an ExpressJS app:
+// https://expressjs.com/en/4x/api.html
+const app = require("express")();
+
+// We'll enable CORS support to allow the function to be invoked
+// from our app client-side.
+app.use(require("cors")({ origin: true }));
+
+// Then we'll also use a special 'getFirebaseUser' middleware which
+// verifies the Authorization header and adds a `user` field to the
+// incoming request:
+// https://gist.github.com/abehaskins/832d6f8665454d0cd99ef08c229afb42
+app.use(getFirebaseUser);
+
+// Add a route handler to the app to generate the secured key
+app.get("/", (req, res) => {
+  // Create the params object as described in the Algolia documentation:
+  // https://www.algolia.com/doc/guides/security/api-keys/#generating-api-keys
+  const params = {
+    // This filter ensures that only documents where author == user_id will be readable
+    filters: `private:false OR author:${req.user.user_id}`,
+    // We also proxy the user_id as a unique token for this key.
+    userToken: req.user.user_id
+  };
+
+  // Call the Algolia API to generate a unique key based on our search key
+  const key = client.generateSecuredApiKey(ALGOLIA_SEARCH_KEY, params);
+
+  // Then return this key as {key: '...key'}
+  res.json({ key });
+});
+
+// Finally, pass our ExpressJS app to Cloud Functions as a function
+// called 'getSearchKey';
+exports.getSearchKey = functions.https.onRequest(app);
+// [END get_algolia_user_token]
