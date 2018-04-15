17c3f507ad4b7b70d7d9b6fb5bb82b568d86475e
Thiago Vicente <thiagovice@gmail.com>
2018-04-15 13:11:16 -0400

    Switch to preact

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+const rewirePreact = require("react-app-rewire-preact");
+
+/* config-overrides.js */
+module.exports = function override(config, env) {
+  console.log("⚡ build with Preact");
+  config = rewirePreact(config, env);
+
+  return config;
+};
