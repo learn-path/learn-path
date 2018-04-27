66dc9321342cafc96069a27b5c947ce9903fd42d
Thiago Vicente <thiagovice@gmail.com>
2018-04-26 22:41:47 -0400

    Disable preact while fixing defaultValue

--- config-overrides.js
+++ config-overrides.js
@@ -2,8 +2,8 @@ const rewirePreact = require("react-app-rewire-preact");
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
-  console.log("⚡ build with Preact");
-  config = rewirePreact(config, env);
+  // console.log("⚡ build with Preact");
+  // config = rewirePreact(config, env);
 
   return config;
 };
